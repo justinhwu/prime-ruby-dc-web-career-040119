c5baf3ad13f963610312ae29b35b47b3f7c5f2d1
Justin Wu <justinhwu95@gmail.com>
2019-03-15 21:26:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num == 1
+    return false
+  elsif num < 1
+    return false
+  elsif num == 2
+    return true
+  end
+
+    (2..num-1).each do |divider|
+      if (num%divider==0)
+        return false
+      end
+    end
+
+  true
+end
