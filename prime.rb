4927430cb40023b543570d8036da7adef7b4d7fb
Justin Wu <justinhwu95@gmail.com>
2019-03-15 21:12:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ require 'prime'
 def prime?(num)
   if num == 1
     return true
-    elsif (num.prime==true)
+    elsif (Prime.num==true)
     return true
     elsif (num.prime==false)
     return false
